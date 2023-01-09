a787dd1bdfbd27a9b7788c800483789f5b7f7536
kimthunbunly <kimthunbunly.rupp@gmail.com>
2023-01-09 21:46:41 +0700

    fix(settled-detail): add 3 column and update style

--- pages/settled/index.vue
+++ pages/settled/index.vue
@@ -194,4 +194,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.v-data-table.row-height-50 td {
+  height: 38px !important;
+  text-align: center !important;
+  font-size: 14px !important;
+}
+.v-data-table.row-height-50 th {
+  height: 38px !important;
+  text-align: center !important;
+  color: #1976d2 !important;
+  font-size: 14px !important;
+}
+.v-data-table.row-height-50 {
+  box-sizing: initial !important;
+}
+</style>
